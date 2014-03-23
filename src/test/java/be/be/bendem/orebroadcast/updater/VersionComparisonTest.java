d013cc5001dfb8fb371af61ca919123fb8f8b9b3
bendem <online@bendem.be>
2014-03-24 00:28:35 +0100

    Add / optimize tests

--- src/test/java/be/be/bendem/orebroadcast/updater/VersionComparisonTest.java
+++ src/test/java/be/be/bendem/orebroadcast/updater/VersionComparisonTest.java
@@ -25,24 +25,50 @@ public class VersionComparisonTest {
     }
 
     @Test
+    public void testVersionDecomposition() {
+        String version;
+        version = "1.2.3";
+        Assert.assertEquals("Major version of " + version, 1, Version.getMainVersion(version));
+        Assert.assertEquals("Minor version of " + version, 2, Version.getMinorVersion(version));
+        Assert.assertEquals("Version revision of " + version, 3, Version.getVersionRevision(version));
+
+        version = "1.2";
+        Assert.assertEquals("Major version of " + version, 1, Version.getMainVersion(version));
+        Assert.assertEquals("Minor version of " + version, 2, Version.getMinorVersion(version));
+        Assert.assertEquals("Version revision of " + version, 3, Version.getVersionRevision(version));
+    }
+
+    @Test
     public void testNewestVersion() {
+        if(version1.equals(version2)) {
+            return;
+        }
         Version ver1 = new Version(version1);
         Version ver2 = new Version(version2);
-        String expected = version1 + " should be a " + (result ? "older" : "newer") + " version then " + version2 + ".";
+        String expected = ver1.getVersion() + " should be a" + (result ? "n older" : " newer") + " version then " + ver2.getVersion() + ".";
         Assert.assertEquals(expected, result, ver1.isNewer(ver2));
+        // Test the contrary
+        expected = ver2.getVersion() + " should be a" + (!result ? "n older" : "newer") + " version then " + ver1.getVersion() + ".";
+        Assert.assertEquals(expected, !result, ver2.isNewer(ver1));
+    }
+
+    @Test
+    public void testEqualsVersion() {
+        if(!version1.equals(version2)) {
+            return;
+        }
+        Version ver1 = new Version(version1);
+        Version ver2 = new Version(version2);
+        String expected = version1 + " compared to " + version2 + " should be always false.";
+        Assert.assertEquals(expected, false, ver1.isNewer(ver2));
+        Assert.assertEquals(expected, false, ver2.isNewer(ver1));
     }
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
         Collection<Object[]> data = new ArrayList<>();
         data.add(new Object[] {
-            "1.2", "1.2", false
-        });
-        data.add(new Object[] {
             "1.1", "1.2", true
-        });
-        data.add(new Object[] {
-            "1.2", "1.1", false
         });
 
         data.add(new Object[] {
@@ -51,43 +77,29 @@ public class VersionComparisonTest {
         data.add(new Object[] {
             "1.2-BETA", "1.1-BETA", false
         });
-        data.add(new Object[] {
-            "1.1-BETA", "1.2-BETA", true
-        });
 
         data.add(new Object[] {
             "1.2-BETA", "1.1", false
-        });
-        data.add(new Object[] {
-            "1.1", "1.2-BETA", true
         });
 
         data.add(new Object[] {
             "1.1-BETA", "1.2", true
         });
-        data.add(new Object[] {
-            "1.2", "1.1-BETA", false
-        });
 
         data.add(new Object[] {
             "1.2.1", "1.2", false
-        });
-        data.add(new Object[] {
-            "1.2", "1.2.1", true
         });
 
         data.add(new Object[] {
             "1.1.1", "1.2", true
         });
-        data.add(new Object[] {
-            "1.2", "1.1.1", false
-        });
 
         data.add(new Object[] {
             "1.1", "1.2.1", true
         });
+
         data.add(new Object[] {
-            "1.2.1", "1.1", false
+            "1.2.2", "1.0.3-BETA", false
         });
 
         return data;
