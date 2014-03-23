5c6850eedcc43cbe2f58d9746edc09efc1ecea0c
bendem <online@bendem.be>
2014-03-23 19:41:01 +0100

    Add version translation stuff to the updater

--- src/main/java/be/bendem/orebroadcast/updater/Updater.java
+++ src/main/java/be/bendem/orebroadcast/updater/Updater.java
@@ -11,15 +11,21 @@ import org.bukkit.entity.Player;
  */
 public class Updater {
 
-    public static final int PROJECT_ID = 72299;
-    public static final String API_URL = "https://api.curseforge.com/servermods/files?projectIds=";
-    public static final String UPDATE_MESSAGE = ChatColor.GOLD + "A new version is available, type " + ChatColor.BOLD
-        + "/ob update" + ChatColor.RESET + ChatColor.GOLD + " to download it";
+    public static final int    PROJECT_ID     = 72299;
+    public static final String API_URL        = "https://api.curseforge.com/servermods/files?projectIds=";
+    public static final String UPDATE_MESSAGE = ChatColor.GOLD + "A new version is available, type "
+            + ChatColor.BOLD + "/ob update" + ChatColor.RESET + ChatColor.GOLD + " to download it";
 
     private final OreBroadcast plugin;
+    private final String       pluginName;
+    private final String       currentVersion;
+    private final Channel      updateChannel;
 
     public Updater(OreBroadcast plugin) {
         this.plugin = plugin;
+        this.pluginName = plugin.getName();
+        this.currentVersion = plugin.getDescription().getVersion();
+        this.updateChannel = plugin.getConfig().getBoolean("use-beta", false) ? Channel.Beta : Channel.Release;
     }
 
     public boolean checkNewVersion() {
@@ -44,6 +50,26 @@ public class Updater {
 
     public void notifyOp(Player op) {
         op.sendMessage(UPDATE_MESSAGE);
+    }
+
+    public Channel getChannel(String version) {
+        return version.contains("BETA") ? Channel.Beta : Channel.Release;
+    }
+
+    public Channel getChannel() {
+        return getChannel(currentVersion);
+    }
+
+    public String getVersionNumber(String version) {
+        return version.replace("-BETA", "").replace(pluginName + "-", "");
+    }
+
+    public String getVersionNumber() {
+        return getVersionNumber(currentVersion);
+    }
+
+    enum Channel {
+        Beta, Release
     }
 
 }
